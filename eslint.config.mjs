ec959c89391087c32d713d154b66f98c63335672
Oksana Danilova <ksanamilitia@mail.ru>
2025-01-25 16:12:07 +0800

    fix app.test

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -12,6 +12,7 @@ export default [
   {
     rules: {
       "no-unused-vars": "warn",
+      "linebreak-style": "off",
     },
   },
   {
--- src/js/__tests__/app.test.js
+++ src/js/__tests__/app.test.js
@@ -1,7 +1,3 @@
-/* eslint-disable jest/expect-expect */
-/* eslint-disable jest/valid-title */
-test("Test", () => {
-  const expected = 4 * 4;
-  const received = 16;
-  expected(received).toBe(expected);
+test("basic test", () => {
+  expect(true).toBe(true);
 });
